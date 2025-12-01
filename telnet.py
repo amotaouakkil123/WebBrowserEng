76ea5b868ba16a716f80efde431bc83f164a7a8b
amotaouakkil123 <adam.motaouakkil@mail.mcgill.ca>
2025-12-01 17:16:48 -0500

    Finish up Chapter 3 about Fonts

--- telnet.py
+++ telnet.py
@@ -1,6 +1,7 @@
 import socket
 import ssl
 import tkinter
+import tkinter.font
 
 class URL:
     def __init__(self, url):
@@ -76,9 +77,10 @@ class Browser:
         )
         self.width = 800
         self.height = 600
-        self.canvas.pack(expand=True)
-        self.window.bind("<Configure>", self.onResize)
+        self.canvas.pack()
         
+        self.display_list = []
+
         self.scroll = 0
         self.window.bind("<Down>", self.scrollDown)
         self.window.bind("<Up>", self.scrollUp)
@@ -87,26 +89,18 @@ class Browser:
 
     def load(self, url):
         body = url.request()
-        text = lex(body)
-        self.display_list = layout(text)
+        tokens = lex(body)
+        self.display_list = Layout(tokens).display_list
         self.draw()
 
     def draw(self):
         self.canvas.delete("all")
-        for x, y, c in self.display_list:
+        for x, y, c, f in self.display_list:
             if y > self.scroll + self.height: continue
             if y + VSTEP < self.scroll: continue
-            self.canvas.create_text(x, y - self.scroll, text=c)
+            self.canvas.create_text(x, y - self.scroll, text=c, anchor='nw', font=f)
 
     SCROLL_STEP = 100
-
-    def onResize(self, e):
-        wScale = float(e.width) / self.width
-        hScale = float(e.height) / self.height
-        self.width = e.width
-        self.height = e.height
-        self.window.config(width=self.width, height=self.height)
-        self.canvas.scale("all", 0, 0, wScale, hScale)
 
     def scrollDown(self, e):
         self.scroll += Browser.SCROLL_STEP
@@ -124,34 +118,119 @@ class Browser:
         self.scroll -= e.y
         self.draw()
 
-def layout(text):
-    display_list = []
-    cursor_x, cursor_y = HSTEP, VSTEP
-    for c in text:
-        display_list.append((cursor_x, cursor_y, c))
-        cursor_x += HSTEP
-            
-        if cursor_x >= WIDTH - HSTEP:
-            cursor_y += VSTEP
-            cursor_x = HSTEP
+class Text:
+    def __init__(self, text):
+        self.text = text
 
-        if c == '\n':
-            cursor_y += VSTEP
-            cursor_x = HSTEP
+class Tag:
+    def __init__(self, tag):
+        self.tag = tag
 
-    return display_list
+FONTS = {}
+
+def get_font(size, weight, style):
+    key = (size, weight, style)
+    if key not in FONTS:
+        font = tkinter.font.Font(size=size, 
+                                 weight=weight,
+                                 slant=style)
+        label = tkinter.Label(font=font)
+        FONTS[key] = label
+    return FONTS[key][0]
+
+class Layout:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.display_list = []
+
+        self.cursor_x = HSTEP
+        self.cursor_y = VSTEP
+
+        self.weight = tkinter.font.NORMAL
+        self.style = tkinter.font.ROMAN
+
+        self.size = 12
+        self.line = []
+
+        for tok in tokens:
+            self.token(tok)
+
+        self.flush()
+
+    def token(self, tok):
+        if isinstance(tok, Text):
+            for word in tok.text.split():
+                self.word(word)
+        
+        elif tok.tag == "i":
+            self.style = tkinter.font.ITALIC
+        elif tok.tag == "/i":
+            self.style = tkinter.font.ROMAN
+        elif tok.tag == "b":
+            self.weight = tkinter.font.BOLD
+        elif tok.tag == "/b":
+            self.weight = tkinter.font.NORMAL
+        elif tok.tag == "small":
+            self.size -= 2
+        elif tok.tag == "/small":
+            self.size += 2
+        elif tok.tag == "big":
+            self.size += 4
+        elif tok.tag == "/big":
+            self.size -= 4
+        elif tok.tag == "br":
+            self.flush()
+        elif tok.tag == "/p":
+            self.flush()
+            self.cursor_y += VSTEP
+
+    def word(self, word):
+
+        font = get_font(self.size, self.weight, self.style)
+        w = font.measure(word)
+
+        if self.cursor_x + w > WIDTH - HSTEP:
+            self.flush()
+
+        self.display_list.append((self.cursor_x, word, font))
+        self.cursor_x += w + font.measure(" ")
+
+    def flush(self):
+        if not self.line: return
+        metrics = [font.metrics() for _, _, font, in self.line]
+        max_ascent = max([metric["ascent"] for metric in metrics])
+        baseline = self.cursor_y + 1.25 * max_ascent
+
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
+
+        max_descent = max([metric["descent"] for metric in metrics])
+        self.cursor_y = baseline + 1.25 * max_descent
+        
+        self.cursor_x = HSTEP
+        self.line = []
 
 def lex(body):
-    tex = ""
+    out = []
+    buffer = ""
     in_tag = False
     for c in body:
         if c == "<":
             in_tag = True
+            if buffer: out.append(Text(buffer))
+            buffer = ""
         elif c == ">":
             in_tag = False
-        elif not in_tag:
-            tex += c
-    return tex
+            out.append(Tag(buffer))
+            buffer = ""
+        else:
+            buffer += c
+
+    if not in_tag and buffer:
+        out.append(Text(buffer))
+
+    return out
 
 if __name__ == "__main__":
     import sys
